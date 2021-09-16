4d44b1c6d474d9ce5c29c4151f58420142104e73
Claire-Cecile Juhasz <ccjuhasz@campus.univ.run>
2021-09-16 14:18:30 +0400

    # Keeping only grounded adults and adults present on colonies

--- PNB-Biometrics_explo.r
+++ PNB-Biometrics_explo.r
@@ -1,78 +1,79 @@
 # pnb <- read.table('C:/Users/Etudiant/Desktop/SMAC/Projet_publi/1-PNB_Methodo_colony/DATA/PNB_adult_biometrics.txt', sep = '\t', dec = ',', h = T)
 
-pnb <- read.table('C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/1-PNB_Methodo_colony/DATA/PNB_adult_biometricsgrounded_vs_colonies.txt',
+pnb <- read.table('C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/1-PNB_Methodo_colony/DATA/PNB_biometrics_grounded_vs_colonies.txt',
                   sep = '\t',
                   dec = ',',
                   h = T)
 pnb
+summary(pnb)
+
+table(pnb$age, pnb$origin)
+pnb$age[pnb$age == 'ad'] <- 'Ad'
 
 library(tidyverse)
 library(ggpubr)
 library(rstatix)
 library(lubridate)
 
+# Adulte selection for biometrics comparaison
+pnb.ad <- pnb[pnb$age == 'Ad',]
 
-pnb$date <- strptime(as.character(pnb$date), '%d/%m/%Y')
+# Completion of month and year variables
+pnb.ad$date <- strptime(as.character(pnb.ad$date), '%d/%m/%Y')
 
 Sys.setlocale('LC_ALL', 'English')
-pnb$month <- month(pnb$date, label = T, abbr = F)
-pnb$year <- year(pnb$date)
+pnb.ad$month <- month(pnb.ad$date, label = T, abbr = F)
+pnb.ad$year <- year(pnb.ad$date)
 
 for (i in c('TA', 'AP', 'LC', 'HC', 'CR', 'weight')){
-print(pnb %>% group_by(origin) %>% get_summary_stats(i, type = 'common'))}
+print(pnb.ad %>% group_by(origin) %>% get_summary_stats(i, type = 'common'))}
 
 for (i in c('TA', 'AP', 'LC', 'HC', 'CR', 'weight')){
-  print(ggboxplot(pnb, x = 'origin',
+  print(ggboxplot(pnb.ad, x = 'origin',
             y = i))}
 
 
 # Data distribution
   # Weight
-hist(pnb$weight, breaks = 20)
-shapiro.test(pnb$weight) # normality test --> OK
+hist(pnb.ad$weight, breaks = 20)
+shapiro.test(pnb.ad$weight) # normality test --> OK
 
-hist(pnb$weight[pnb$origin == 'colony'], breaks = 20)
-shapiro.test(pnb$weight[pnb$origin == 'colony']) # normality test --> NOT OK
+hist(pnb.ad$weight[pnb.ad$origin == 'colony'], breaks = 20)
+shapiro.test(pnb.ad$weight[pnb.ad$origin == 'colony']) # normality test --> NOT OK
 
-hist(pnb$weight[pnb$origin == 'grounded'], breaks = 20)
-shapiro.test(pnb$weight[pnb$origin == 'grounded']) # normality test --> NOT OK
+hist(pnb.ad$weight[pnb.ad$origin == 'grounded'], breaks = 20)
+shapiro.test(pnb.ad$weight[pnb.ad$origin == 'grounded']) # normality test --> NOT OK
 
-t.test(pnb$weight)
-kruskal.test(pnb$weight ~ as.factor(pnb$origin))
+kruskal.test(pnb.ad$weight ~ as.factor(pnb.ad$origin)) # Significant difference btw grounded and colonies
 
   # AP
 
-hist(pnb$AP, breaks = 20)
-shapiro.test(pnb$AP) # normality test --> NOT OK
+hist(pnb.ad$AP, breaks = 20)
+shapiro.test(pnb.ad$AP) # normality test --> NOT OK
 
-hist(pnb$AP[pnb$origin == 'colony'], breaks = 20)
-shapiro.test(pnb$AP[pnb$origin == 'colony']) # normality test --> OK
+hist(pnb.ad$AP[pnb.ad$origin == 'colony'], breaks = 20)
+shapiro.test(pnb.ad$AP[pnb.ad$origin == 'colony']) # normality test --> OK
 
-hist(pnb$AP[pnb$origin == 'grounded'], breaks = 20)
-shapiro.test(pnb$AP[pnb$origin == 'grounded']) # normality test --> OK
+hist(pnb.ad$AP[pnb.ad$origin == 'grounded'], breaks = 20)
+shapiro.test(pnb.ad$AP[pnb.ad$origin == 'grounded']) # normality test --> OK
 
-t.test(pnb$AP)
-kruskal.test(pnb$AP ~ as.factor(pnb$origin))
+t.test(pnb.ad$AP ~ pnb.ad$origin) # Significant difference btw grounded and colonies
 
   # TA
 
-hist(pnb$TA, breaks = 20)
-shapiro.test(pnb$TA) # normality test --> NOT OK
+hist(pnb.ad$TA, breaks = 20) # Possibly one outlier
+hist(pnb.ad$TA[pnb.ad$origin == 'colony' & pnb.ad$TA > 31], breaks = 20)
 
-hist(pnb$TA[pnb$origin == 'colony'], breaks = 20)
-hist(pnb$TA[pnb$origin == 'colony' & pnb$TA > 31])
+boxplot(pnb.ad$TA[pnb.ad$origin == 'colony'])
+boxplot(pnb.ad$TA[pnb.ad$origin == 'colony' & pnb.ad$TA > 31])
 
-boxplot(pnb$TA[pnb$origin == 'colony'])
-boxplot(pnb$TA[pnb$origin == 'colony' & pnb$TA > 31])
+# shapiro.test(pnb$TA[pnb$origin == 'colony']) # normality test --> NOT OK --> OUTLIER ?
+shapiro.test(pnb.ad$TA[pnb.ad$origin == 'colony' & pnb.ad$TA > 31]) # normality test --> OK without ONE OUTLIER
 
-shapiro.test(pnb$TA[pnb$origin == 'colony']) # normality test --> NOT OK --> OUTLIER ?
-shapiro.test(pnb$TA[pnb$origin == 'colony' & pnb$TA > 31]) # normality test --> OK without ONE OUTLIER
+hist(pnb.ad$TA[pnb.ad$origin == 'grounded'], breaks = 20)
+shapiro.test(pnb.ad$TA[pnb.ad$origin == 'grounded']) # normality test --> OK
 
-hist(pnb$TA[pnb$origin == 'grounded'], breaks = 20)
-shapiro.test(pnb$TA[pnb$origin == 'grounded']) # normality test --> OK
-
-t.test(pnb$TA)
-kruskal.test(pnb$TA ~ as.factor(pnb$origin))
+t.test(pnb.ad$TA[pnb.ad$TA > 31]~ as.factor(pnb.ad$origin[pnb.ad$TA > 31])) # Significant difference btw grounded and colonies
 
 # Growth curve per group
 # Gompertz equation
