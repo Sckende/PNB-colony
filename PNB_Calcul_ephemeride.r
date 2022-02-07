0c9702e83d158628cca077319fdd0f1c08e4c9aa
Sckende <clrccljuhasz@gmail.com>
2022-02-07 13:25:50 +0400

    # Delay btw sunrise & deployment

--- PNB_Calcul_ephemeride.r
+++ PNB_Calcul_ephemeride.r
@@ -34,7 +34,39 @@ eph <- apply(ephem[, c("sunset",
 ephem2 <- cbind(ephem[, c("date", "lat", "lon")],
                 eph)
 head(ephem2)
+summary(ephem2)
 
-write.table(ephem2,
-            file = "C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/1-PNB_Methodo_colony/DATA/PNB_ephemeride_2015-2022.txt",
-            row.names = F)
+# write.table(ephem2,
+#             file = "C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/1-PNB_Methodo_colony/DATA/PNB_ephemeride_2015-2022.txt",
+#             row.names = F)
+
+# Calculating delay betw 5am and sunrise in minutes ####
+########################################################
+
+head(ephem2)
+date_sunrise <- as.POSIXct(paste(ephem2$date,
+                                 ephem2$sunrise,
+                                 sep = " "),
+                           format = "%Y-%m-%d %H:%M")
+date_sunrise
+head(ephem2$sunrise)
+head(date_sunrise)
+
+hour(date_sunrise)
+minute(date_sunrise)
+
+ephem2$sunrise_min <- hour(date_sunrise) * 60 + minute(date_sunrise)
+head(ephem2)
+tail(ephem2)
+
+# -----> delay btw sunrise and 5 AM = 300 min
+delay <- ephem2$sunrise_min - 300
+summary(delay)
+summary(ephem2$sunrise_min)
+
+# Calculating delay betw 4am and sunrise in minutes ####
+########################################################
+# -----> delay btw sunrise and 4 AM = 240 min
+delay2 <- ephem2$sunrise_min - 240
+summary(delay2)
+summary(ephem2$sunrise_min)
